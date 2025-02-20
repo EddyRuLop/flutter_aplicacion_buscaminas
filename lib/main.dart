5ca2f4bcf91ace4cdd409b2165282ad7df47526d
Eduardo Ruiz <eduardoyahel@gmail.com>
2025-02-19 21:43:57 -0600

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,387 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/bomb.dart';
+import 'package:flutter_application_1/numberbox.dart';
 
-void main() {
-  runApp(const MyApp());
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _HomePageState extends State<HomePage> {
+// variables
 
-  // This widget is the root of your application.
+  int numberOfSquares = 9 * 9;
+  int numberInEachRow = 9;
+
+  //numero de bombas reveladas
+  var squareStatus = [];
+
+  //locaciones de bomba
+
+  final List<int> bombLocation = [
+    4,
+    5,
+    6,
+    32,
+    18,
+    20,
+    25,
+    41,
+    42,
+    43,
+    61,
+  ];
+
+  bool bombsRevealed = false;
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  void initState() {
+    super.initState();
+
+    //inicializar cuando hay 0 bombas y ninguna es revelada
+    for (int i = 0; i < numberOfSquares; i++) {
+      squareStatus.add([0, false]);
+    }
+    scanBombs();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void restartGame() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      bombsRevealed = false;
+      for (int i = 0; i < numberOfSquares; i++) {
+        squareStatus[i][1] = false;
+      }
     });
   }
 
+  void revealBoxNumbers(int index) {
+    // revela cuando los numeros son 1,2,3...
+    if (squareStatus[index][0] != 0) {
+      setState(() {
+        squareStatus[index][1] = true;
+      });
+    }
+
+    // cuando es 0
+    else if (squareStatus[index][0] == 0) {
+      // revelar los 8 cuadros al rededor, a menos que este en una pared
+      setState(() {
+        squareStatus[index][1] = true;
+
+        //revelar el cuadro izquierdo, a menos que este en una pared
+        if (index % numberInEachRow != 0) {
+          // cuando no se ha revelado y es 0
+          if (squareStatus[index - 1][0] == 0 &&
+              squareStatus[index - 1][1] == false) {
+            revealBoxNumbers(index - 1);
+          }
+
+//revelar el cuadro izquierdo
+
+          squareStatus[index - 1][1] = true;
+        }
+
+        //revelar el cuadro derecho, a menos que este en una pared
+        if (index % numberInEachRow != 0 && index >= numberInEachRow) {
+          // cuando no se ha revelado y es 0
+          if (squareStatus[index - 1 - numberInEachRow][0] == 0 &&
+              squareStatus[index - 1 - numberInEachRow][1] == false) {
+            revealBoxNumbers(index - 1 - numberInEachRow);
+          }
+
+          squareStatus[index - 1 - numberInEachRow][1] = true;
+        }
+
+        //revelar el cuadro superior, a menos que este en una pared
+        if (index >= numberInEachRow) {
+          // cuando no se ha revelado y es 0
+          if (squareStatus[index - numberInEachRow][0] == 0 &&
+              squareStatus[index - numberInEachRow][1] == false) {
+            revealBoxNumbers(index - numberInEachRow);
+          }
+
+          squareStatus[index - numberInEachRow][1] = true;
+        }
+
+        //revelar el cuadro superior, a menos que este en una pared
+        if (index >= numberInEachRow &&
+            index % numberInEachRow != numberInEachRow - 1) {
+          // cuando no se ha revelado y es 0
+          if (squareStatus[index + 1 - numberInEachRow][0] == 0 &&
+              squareStatus[index + 1 - numberInEachRow][1] == false) {
+            revealBoxNumbers(index + 1 - numberInEachRow);
+          }
+
+          squareStatus[index + 1 - numberInEachRow][1] = true;
+        }
+
+        //revelar el cuadro superior, a menos que este en una pared
+        if (index % numberInEachRow != numberInEachRow - 1) {
+          // cuando no se ha revelado y es 0
+          if (squareStatus[index + 1][0] == 0 &&
+              squareStatus[index + 1][1] == false) {
+            revealBoxNumbers(index + 1);
+          }
+
+          squareStatus[index + 1][1] = true;
+        }
+
+        //revelar el cuadro superior, a menos que este en una pared
+        if (index < numberOfSquares - numberInEachRow &&
+            index % numberInEachRow != numberInEachRow - 1) {
+          // cuando no se ha revelado y es 0
+          if (squareStatus[index + 1 + numberInEachRow][0] == 0 &&
+              squareStatus[index + 1 + numberInEachRow][1] == false) {
+            revealBoxNumbers(index + 1 + numberInEachRow);
+          }
+
+          squareStatus[index + 1 + numberInEachRow][1] = true;
+        }
+
+        //revelar el cuadro superior, a menos que este en una pared
+        if (index < numberOfSquares - numberInEachRow) {
+          // cuando no se ha revelado y es 0
+          if (squareStatus[index + numberInEachRow][0] == 0 &&
+              squareStatus[index + numberInEachRow][1] == false) {
+            revealBoxNumbers(index + numberInEachRow);
+          }
+
+          squareStatus[index + numberInEachRow][1] = true;
+        }
+
+        //revelar el cuadro superior, a menos que este en una pared
+        if (index < numberOfSquares - numberInEachRow &&
+            index % numberInEachRow != 0) {
+          // cuando no se ha revelado y es 0
+          if (squareStatus[index - 1 + numberInEachRow][0] == 0 &&
+              squareStatus[index - 1 + numberInEachRow][1] == false) {
+            revealBoxNumbers(index - 1 + numberInEachRow);
+          }
+
+          squareStatus[index - 1 + numberInEachRow][1] = true;
+        }
+      });
+    }
+  }
+
+  void scanBombs() {
+    for (int i = 0; i < numberOfSquares; i++) {
+      //cuando no hay bombas al rededor inicializar
+      int numberOfBombsAround = 0;
+
+      // check square to the left, unless it is in the first column
+
+      if (bombLocation.contains(i - 1) && i % numberInEachRow != 0) {
+        numberOfBombsAround++;
+      }
+
+      // check square to the left, unlesss it is in he fisrst column
+      if (bombLocation.contains(i - 1 - numberInEachRow) &&
+          i % numberInEachRow != 0 &&
+          i >= numberInEachRow) {
+        numberOfBombsAround++;
+      }
+
+      if (bombLocation.contains(i - numberInEachRow) && i >= numberInEachRow) {
+        numberOfBombsAround++;
+      }
+
+      // check square to the left, unlesss it is in he fisrst column
+      if (bombLocation.contains(i + 1 - numberInEachRow) &&
+          i >= numberInEachRow &&
+          i % numberInEachRow != numberInEachRow - 1) {
+        numberOfBombsAround++;
+      }
+
+      // check square to the left, unlesss it is in he fisrst column
+      if (bombLocation.contains(i + 1) &&
+          i % numberInEachRow != numberInEachRow - 1) {
+        numberOfBombsAround++;
+      }
+
+      // check square to the left, unlesss it is in he fisrst column
+      if (bombLocation.contains(i + 1 + numberInEachRow) &&
+          i % numberInEachRow != numberInEachRow - 1 &&
+          i < numberOfSquares - numberInEachRow) {
+        numberOfBombsAround++;
+      }
+
+      // check square to the left, unlesss it is in he fisrst column
+      if (bombLocation.contains(i + numberInEachRow) &&
+          i < numberOfSquares - numberInEachRow) {
+        numberOfBombsAround++;
+      }
+
+      // check square to the left, unlesss it is in he fisrst column
+      if (bombLocation.contains(i - 1 + numberInEachRow) &&
+          i < numberOfSquares - numberInEachRow &&
+          i % numberInEachRow != 0) {
+        numberOfBombsAround++;
+      }
+
+      //add total number of bombs aroun to square status
+
+      setState(() {
+        squareStatus[i][0] = numberOfBombsAround;
+      });
+    }
+  }
+
+  void playerLost() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.grey[800],
+            title: Center(
+              child: Text(
+                "Has perdido",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            actions: [
+              MaterialButton(
+                  color: Colors.grey[100],
+                  onPressed: () {
+                    restartGame();
+                    Navigator.pop(context);
+                  },
+                  child: Icon(Icons.refresh))
+            ],
+          );
+        });
+  }
+
+  void playerWon() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.grey[700],
+          title: Center(
+            child: Text(
+              "HAS GANADO",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          actions: [
+            MaterialButton(
+                color: Colors.grey[100],
+                onPressed: () {
+                  restartGame();
+                  Navigator.pop(context);
+                },
+                child: Icon(Icons.refresh))
+          ],
+        );
+      },
+    );
+  }
+
+  void checkWinner() {
+    int unrevealedBoxes = 0;
+    for (int i = 0; i < numberOfSquares; i++) {
+      if (squareStatus[i][1] == false) {
+        unrevealedBoxes++;
+      }
+    }
+
+    if (unrevealedBoxes == bombLocation.length) {
+      playerWon();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.grey[200],
+      body: Column(
+        children: [
+          //Menu y contadores
+          Container(
+            height: 150,
+            color: Colors.grey,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //contador de bombas
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(bombLocation.length.toString(),
+                        style: TextStyle(fontSize: 40)),
+                    Text("Bombas"),
+                  ],
+                ),
+
+                //boton de volver a cargar el juego
+                GestureDetector(
+                  onTap: restartGame,
+                  child: Card(
+                    color: Colors.grey[700],
+                    child: Icon(Icons.refresh, color: Colors.white, size: 40),
+                  ),
+                ),
+
+                Card(
+                  color: Colors.grey[700],
+                  child: Icon(Icons.refresh, color: Colors.white, size: 40),
+                ),
+
+                // Tiempo
+
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('0', style: TextStyle(fontSize: 40)),
+                    Text("Tiempo"),
+                  ],
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+
+          //grid
+          Expanded(
+            child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: numberOfSquares,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: numberInEachRow),
+                itemBuilder: (context, index) {
+                  if (bombLocation.contains(index)) {
+                    return MyBomb(
+                        revealed: bombsRevealed,
+                        function: () {
+                          setState(() {
+                            bombsRevealed = true;
+                          });
+                          playerLost();
+                          //player tapped the bomb, so player loses
+                        });
+                  } else {
+                    return MyNumberBox(
+                        child: squareStatus[index][0],
+                        revealed: squareStatus[index][1],
+                        function: () {
+                          //reveal current box
+                          revealBoxNumbers(index);
+                          checkWinner();
+                        });
+                  }
+                }),
+          ),
+
+          //Marca personal
+
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: Text("Creado Por: Eduardo Ruiz"),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
